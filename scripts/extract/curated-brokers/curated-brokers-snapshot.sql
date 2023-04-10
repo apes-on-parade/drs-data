8549692b87815737f1a8c6f6bfc6447b92c691bb
Apes on Parade <88012763+apes-on-parade@users.noreply.github.com>
2023-04-10 03:37:12 +0000

    Brokers for wix dynamic content

--- scripts/extract/curated-brokers/curated-brokers-snapshot.sql
+++ scripts/extract/curated-brokers/curated-brokers-snapshot.sql
@@ -1,29 +1,47 @@
 CREATE OR REPLACE TABLE `apes-on-parade-default.src_first_party.curated_brokers_snapshot` AS
 SELECT
-  name,
-  country,
-  SPLIT(languages, ", ") as languages,
-  CASE hasDirectDrs
-    WHEN "Yes" THEN TRUE
-    WHEN "No" THEN FALSE
-    END AS hasDirectDrs,
-  CASE requiresExistingAccount
-    WHEN "Yes" THEN TRUE
-    WHEN "No" THEN FALSE
-    END AS requiresExistingAccount,
-  CASE requiresLetterOfInstruction
-    WHEN "Yes" THEN TRUE
-    WHEN "No" THEN FALSE
-    END AS requiresLetterOfInstruction,
-  note,
-  expectedDrsFee,
-  expectedDrsDuration,
-  contactInfoCompletion,
-  phone,
-  email,
-  mailingAddress,
-  website,
-  twitter
+  Name_of_broker as name,
+  Country as country,
+  SPLIT(Language_spoken, ", ") as languages,
+  -- DRS Info
+  STRUCT(
+    CAST(Direct_DRS_available_ AS BOOLEAN) as hasDirect,
+    CAST(Letter_of_instruction_ AS BOOLEAN) as doesRequireLoi,
+    CAST(CS___Required_ AS BOOLEAN) as doesRequireAccount,
+    CASE CS_Account__Number_Mentioned_
+      WHEN "Demands Acc # Disclaimer" THEN TRUE
+      WHEN "Disclaimer" THEN TRUE
+      WHEN "No" THEN FALSE
+      ELSE null
+      END as doesRequestAccount,
+    CASE Reseller_or_Saxo_
+      WHEN "Yes" THEN TRUE
+      WHEN "No" THEN FALSE
+      END as isExpensiveDrs,
+    Notes as note,
+    CASE Transfer_to_IBKR
+      WHEN "Yes" THEN TRUE
+      WHEN "No" THEN FALSE
+      END as transferToIbkr,
+    CASE Transfer_to_another_broker
+      WHEN "Yes" THEN TRUE
+      WHEN "No" THEN FALSE
+      END as transferToOther,
+    CASE Cannot_transfer
+      WHEN "Yes" THEN TRUE
+      WHEN "No" THEN FALSE
+      END as transferUnavailable,
+    Expected_Fee as expectedFee,
+    Duration as expectedDuration
+  ) as drs,
+  -- Contact Info
+  STRUCT(
+    Phone_Number as phone,
+    Email_Address as email,
+    Mailing_address as mailingAddress,
+    Website as website,
+    Twitter as twitter
+  ) as contact
 
 FROM `apes-on-parade-default.src_first_party.curated_brokers_sheet` as b
 
--- scripts/transform/wix/brokers-en.sql
+++ scripts/transform/wix/brokers-en.sql
@@ -1,23 +1,69 @@
+CREATE TEMP FUNCTION enSummaryParagraph(
+    name STRING,
+    country STRING,
+    hasDirect BOOLEAN,
+    doesRequireAccount BOOLEAN,
+    isExpensive BOOLEAN,
+    transferUnavailable BOOLEAN,
+    expectedFee STRING,
+    expectedDuration STRING
+  )
+  RETURNS STRING
+  LANGUAGE js
+  AS r"""
+    if(country=="UM"){country="US"} /*Work around apparent data entry error*/
+    const usness = country == "US" ? "U.S." : "non-U.S."
+    let para = `${name} is a ${usness} broker. `
+    if(country === "US"){
+      if(hasDirect){drs()}
+      if(!hasDirect){
+        if(!transferUnavailable){usTransfer()}
+        if(transferUnavailable){usRepurchase()}
+      }
+    }
+    if(country !== "US"){
+      if(hasDirect === false){
+        if(transferUnavailable === false){nonusTransfer()}
+        if(transferUnavailable === true){nonusRepurchase()}
+      }
+      if(hasDirect === true){
+        if(isExpensive === true){nonusExpensive()}
+        else{
+          if(!doesRequireAccount){drs()}
+          if(doesRequireAccount){nonusPreexisting()}
+        }
+      }
+    }
+    return `<p class="font_8">para</p>`;
 
+    function drs(){para+=`${name} is able to directly DRS shares upon your request.`+(expectedDuration ? `Expect the request to take ${expectedDuration} to process. `:"")+`See our broker-specific guide below for instructions.`}
+    function usTransfer(){para += `${name} cannot DRS transfer, but can transfer to other brokers. To DRS, you will first need to transfer to a broker that can DRS, for example Fidelity. See our <a href="#">Fidelity Route Guide</a>.`}
+    function usRepurchase(){para += `${name} cannot DRS transfer, and cannot transfer to other brokers. To DRS, you will need to close out your position and rebuy with a DRS-capable broker or the transfer agent directly. See our <a href="#">U.S. Sell+Repurchase Guide</a>.`}
+    function nonusTransfer(){para+=`${name} cannot DRS transfer, but can transfer to other brokers. To DRS, you will first need to transfer to a broker that can DRS, for example Interactive Brokers (IBKR). See our <a href="#">IBKR Route Guide</a>.`}
+    function nonusExpensive(){para+=`${name} can DRS transfer, but charges an excessive fee to do so (${expectedFee}). Although you may DRS directly from this broker, we instead recommend first transfering to a broker that can DRS for a reasonable cost, for example Interactive Brokers (IBKR). See our <a href="#">IBKR Route Guide</a>`}
+    function nonusPreexisting(){para+=`This broker requires a pre-existing account at the transfer agent. See our <a href="#">Creating an Initial Account guide</a>`}
+    function nonusRepurchase(){para += `${name} cannot DRS transfer, and cannot transfer to other brokers. To DRS, you will need to close out your position and rebuy with a DRS broker or the transfer agent directly. See our <a href="#">Non-U.S. Sell+Repurchase Guide</a>/`}
+
+  """;
 
 SELECT
-  name,
-  CASE
-  WHEN country = "US" THEN
-    name || " is a US broker. " ||
-    CASE WHEN hasDirectDrs THEN
-      "To DRS transfer from " || name || " you need to..."
-    ELSE
-      name || " cannot DRS transfer. "
-      || "<CASE WHEN can broker transfer THEN ... ELSE ...>"
-    END
-  ELSE
-    name || " is a " || country || " broker." ||
-    CASE WHEN hasDirectDrs THEN
-      "< Can DRS: CASE WHEN is Saxo-related THEN ... ELSE ...>"
-    ELSE
-      "< Can't DRS: CASE WHEN can broker transfer THEN ... ELSE ...>"
-    END
-  END as paragraph
-
+  REGEXP_REPLACE(
+    TO_HEX(SHA1(name)),
+    "^([0-9a-f]{8})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{12})",
+    "\\1-\\2-\\3-\\4-\\5"
+  ) as _id,
+  b.name as title,
+  -- b.drs.note,
+  enSummaryParagraph(
+    name,
+    b.country,
+    b.drs.hasDirect,
+    b.drs.doesRequireAccount,
+    b.drs.isExpensive,
+    b.drs.transferUnavailable,
+    b.drs.expectedFee,
+    b.drs.expectedDuration
+    ) as enSummaryParagraph
 FROM `apes-on-parade-default.src_first_party.curated_brokers_snapshot` as b
+
+-- LIMIT 100
