820b4f0e258ec37a1639a517fe26628f8d37f093
Apes on Parade <88012763+apes-on-parade@users.noreply.github.com>
2023-04-15 06:41:21 +0000

    Issuer and security transforms

--- scripts/transform/core/issuers.sql
+++ scripts/transform/core/issuers.sql
@@ -1,4 +1,3 @@
-
 --CREATE OR REPLACE TABLE `apes-on-parade-default.dist.issuers` AS
 WITH
 	cik_sec as (
@@ -22,7 +21,7 @@ WITH
 			-- Because a 10K doc may contain 2 sentences that describe the holdings, or because the extraction script may have double-extracted a sentence
 			-- we discard any sentences that both specify the same number. However, since there can be multiple sentences that each describe separate classes
 			-- of shares, we do not discard (and later sum) any different numbers from the same filing
-			SELECT DISTINCT cik, filingDate, numHolders
+			SELECT DISTINCT cik, filingDate, numHolders, docUrl
 			FROM `apes-on-parade-default.src_first_party.10k_holders_snapshot`
 			) as unique_holding_numbers
 		GROUP BY 1, 2
@@ -57,36 +56,33 @@ WITH
 		)) as tickers
 		FROM `apes-on-parade-default.drs_data.gorillionaire_tickers` as spreadsheet
 		INNER JOIN `apes-on-parade-default.src_public.sec_cik_tickers` as official
-		ON  official.cik = spreadsheet.cik
-		AND official.exchange = spreadsheet.exchange
-		AND official.ticker = spreadsheet.stockSymbol
+			ON  official.cik = spreadsheet.cik
+			AND official.exchange = spreadsheet.exchange
+			AND official.ticker = spreadsheet.stockSymbol
 		WHERE spreadsheet.cik IS NOT NULL
-		AND (spreadsheet.exchange = "NYSE" OR spreadsheet.exchange="Nasdaq")
+			AND (spreadsheet.exchange = "NYSE" OR spreadsheet.exchange="Nasdaq")
 		GROUP BY 1
 		)
 SELECT
-	latest.cik,
+	cik_sec.cik,
 	COALESCE(cik_gorillionaire.name, cik_sec.name) as name,
 	cik_gorillionaire.tickers,
 	STRUCT(
 		holders.filingDate,
 		holders.totalHolders,
 		holders.docUrls
-		) as holders
-FROM cik_sec,
+		) as holders,
+	cik_sec.is_NN_traded
+FROM cik_sec
+LEFT JOIN cik_gorillionaire
+	ON cik_gorillionaire.cik = cik_sec.cik
 LEFT JOIN cik_latest_holders_filing as latest_holders
+	ON latest_holders.cik = cik_sec.cik
 LEFT JOIN cik_date_holders as holders
 	ON  holders.cik = latest_holders.cik
 	AND holders.filingDate = latest_holders.filingDate
-LEFT JOIN cik_date_doc_urls as docs
-	ON  docs.cik = latest.cik
-	AND docs.filingDate = latest.filingDate
-LEFT JOIN cik_sec
-	ON cik_sec.cik = latest.cik
-LEFT JOIN cik_gorillionaire
-	ON cik_gorillionaire.cik = latest.cik
 
-WHERE cik_sec.is_NN_traded
-
-ORDER BY holders.totalHolders DESC
-LIMIT 25
+-- WHERE cik_sec.is_NN_traded
+--
+-- ORDER BY holders.totalHolders DESC
+-- LIMIT 25
--- scripts/transform/core/securities.sql
+++ scripts/transform/core/securities.sql
@@ -29,7 +29,7 @@ CREATE TEMPORARY FUNCTION stringOverlap(str1 STRING, str2 STRING)
 	""";
 
 
---CREATE OR REPLACE TABLE `apes-on-parade-default.dist.securities` AS
+CREATE OR REPLACE TABLE `apes-on-parade-default.dist.securities` AS
 WITH
 	security_sec as (
 		SELECT
@@ -47,122 +47,143 @@ WITH
 		FROM `apes-on-parade-default.src_public.sec_cik_tickers`
 		GROUP BY 1, 2
 		)
-	--, security_gorillionaire as (
-	-- 	SELECT
-	-- 		spreadsheet.cik as pk2_cik,
-	-- 		spreadsheet.stockSymbol as pk2_ticker,
-	-- 		CASE WHEN MIN(spreadsheet.companyName) <> MAX(spreadsheet.companyName)
-	-- 			THEN ERROR('Non-unique violation: security_gorillionaire.companyName; cik:' || cik || ', ticker:' || stockSymbol)
-	-- 			ELSE MIN(spreadsheet.companyName)
-	-- 			END as issuerName,
-	-- 		CASE WHEN MIN(spreadsheet.exchange) <> MAX(spreadsheet.exchange)
-	-- 			THEN ERROR('Non-unique violation: gorillionaire.exchange; cik:' || cik || ', ticker:' || stockSymbol)
-	-- 			ELSE MIN(spreadsheet.exchange)
-	-- 			END as exchange,
-	-- 		CASE WHEN MIN(spreadsheet.cusip) <> MAX(spreadsheet.cusip)
-	-- 			THEN ERROR('Non-unique violation: gorillionaire.cusip; cik:' || cik || ', ticker:' || stockSymbol)
-	-- 			ELSE MIN(spreadsheet.cusip)
-	-- 			END as cusip,
-	-- 		CASE WHEN MIN(spreadsheet.hasOnlinePurchase) <> MAX(spreadsheet.hasOnlinePurchase)
-	-- 			THEN ERROR('Non-unique violation: gorillionaire.hasOnlinePurchase; cik:' || cik || ', ticker:' || stockSymbol)
-	-- 			ELSE MIN(spreadsheet.hasOnlinePurchase)
-	-- 			END as hasOnlinePurchase,
-	-- 		STRUCT(
-	-- 			CASE WHEN MIN(spreadsheet.dtcMemberId) <> MAX(spreadsheet.dtcMemberId)
-	-- 				THEN ERROR('Non-unique violation: gorillionaire.dtcMemberId; cik:' || cik || ', ticker:' || stockSymbol)
-	-- 				ELSE MIN(spreadsheet.dtcMemberId)
-	-- 				END as dtcMemberId,
-	-- 			CASE WHEN MIN(spreadsheet.transferAgent) <> MAX(spreadsheet.transferAgent)
-	-- 				THEN ERROR('Non-unique violation: gorillionaire.transferAgent; cik:' || cik || ', ticker:' || stockSymbol)
-	-- 				ELSE MIN(spreadsheet.transferAgent)
-	-- 				END as name,
-	-- 			CAST('1900-01-01' AS DATE) as asOfDate
-	-- 			) as transferAgent
-	-- 	FROM `apes-on-parade-default.drs_data.gorillionaire_tickers` as spreadsheet
-	-- 	WHERE spreadsheet.cik IS NOT NULL
-	-- 	GROUP BY 1, 2
-	-- )
--- 	, map_nasdaq_dtc_transfer_agents AS (
--- 		SELECT name as pk1_name,
--- 			CASE WHEN MIN(selected_id) <> MAX(selected_id) THEN ERROR('Non-unique violation: map_nasdaq_dtc_transfer_agents, name:' || name)
--- 				ELSE MIN(selected_id) END as dtcMemberId
--- 		FROM `apes-on-parade-default.src_first_party.map_nasdaq_dtc_transfer_agents`
--- 		GROUP BY 1
--- 		)
--- , security_nasdaq as (
--- 		SELECT
--- 			security_sec.pk2_cik,
--- 			security_sec.pk2_ticker,
--- 		CASE WHEN MIN(nasdaq.CUSIP) <> MAX(nasdaq.CUSIP)
--- 			THEN ERROR('Non-unique violation: nasdaq.CUSIP; cik:' || pk2_cik || ', ticker:' || pk2_ticker)
--- 			ELSE MIN(nasdaq.CUSIP)
--- 			END as cusip,
--- 		CASE WHEN MIN(nasdaq.Description) <> MAX(nasdaq.Description)
--- 			THEN ERROR('Non-unique violation: nasdaq.Description; cik:' || pk2_cik || ', ticker:' || pk2_ticker)
--- 			ELSE MIN(nasdaq.Description)
--- 			END as description,
--- 		CASE WHEN MIN(nasdaq.Transfer_Agent) <> MAX(nasdaq.Transfer_Agent)
--- 			THEN ERROR('Non-unique violation: nasdaq.Transfer_Agent; cik:' || pk2_cik || ', ticker:' || pk2_ticker)
--- 			ELSE ARRAY_AGG(STRUCT(
--- 				ta as name,
--- 				map.dtcMemberId,
--- 				Effective_Date as asOfDate,
--- 				"https://www.nasdaqtrader.com/content/home/help/samplereports/Fundamental_Data_cusip121720.xlsx" as docUrl
--- 				))
--- 			END as claimedTransferAgents
--- 		FROM `apes-on-parade-default.src_unlicensed.nasdaq_fundamentals_sample` as nasdaq
--- 		LEFT JOIN UNNEST([ SPLIT(LOWER(Issue_Name), SUBSTR(LOWER(Description),1,10))[OFFSET(0)] ]) as impliedIssuerName
--- 		INNER JOIN security_sec
--- 			ON  security_sec.exchange = "Nasdaq"
--- 			AND security_sec.pk2_ticker = nasdaq.Symbol
--- 			AND stringOverlap(security_sec.issuerName, impliedIssuerName) > 25
--- 		LEFT JOIN UNNEST(SPLIT(nasdaq.Transfer_Agent, " ; ")) as ta
--- 		LEFT JOIN map_nasdaq_dtc_transfer_agents as map
--- 			ON map.pk1_name = ta
--- 		GROUP BY 1,2
--- 	)
---, security_nyse as (
-	-- SELECT
-	-- 	security_sec.pk2_cik,
-	-- 	security_sec.pk2_ticker,
-	-- 	CASE WHEN MIN(nyse.CUSIP) <> MAX(nyse.CUSIP)
-	-- 		THEN ERROR('Non-unique violation: nyse.CUSIP; cik:' || pk2_cik || ', ticker:' || pk2_ticker)
-	-- 		ELSE MIN(nyse.CUSIP)
-	-- 		END as cusip,
-	-- 	CASE WHEN MIN(nyse.Security_Name) <> MAX(nyse.Security_Name)
-	-- 		THEN ERROR('Non-unique violation: nyse.Security_Name; cik:' || pk2_cik || ', ticker:' || pk2_ticker)
-	-- 		ELSE MIN(nyse.Security_Name)
-	-- 		END as description,
-	-- FROM `apes-on-parade-default.src_unlicensed.nyse_securities` as nyse
-	-- INNER JOIN security_sec
-	-- 	ON  security_sec.exchange = "NYSE"
-	-- 	AND security_sec.pk2_ticker = nyse.Stock_Symbol
-	-- 	AND stringOverlap(security_sec.issuerName, nyse.Issuer_Name) > 25
-	-- GROUP BY 1,2
---)
+	, security_gorillionaire as (
+		SELECT
+			spreadsheet.cik as pk2_cik,
+			spreadsheet.stockSymbol as pk2_ticker,
+			CASE WHEN MIN(spreadsheet.companyName) <> MAX(spreadsheet.companyName)
+				THEN ERROR('Non-unique violation: security_gorillionaire.companyName; cik:' || cik || ', ticker:' || stockSymbol)
+				ELSE MIN(spreadsheet.companyName)
+				END as issuerName,
+			CASE WHEN MIN(spreadsheet.exchange) <> MAX(spreadsheet.exchange)
+				THEN ERROR('Non-unique violation: gorillionaire.exchange; cik:' || cik || ', ticker:' || stockSymbol)
+				ELSE MIN(spreadsheet.exchange)
+				END as exchange,
+			CASE WHEN MIN(spreadsheet.cusip) <> MAX(spreadsheet.cusip)
+				THEN ERROR('Non-unique violation: gorillionaire.cusip; cik:' || cik || ', ticker:' || stockSymbol)
+				ELSE MIN(spreadsheet.cusip)
+				END as cusip,
+			CASE WHEN MIN(spreadsheet.hasOnlinePurchase) <> MAX(spreadsheet.hasOnlinePurchase)
+				THEN ERROR('Non-unique violation: gorillionaire.hasOnlinePurchase; cik:' || cik || ', ticker:' || stockSymbol)
+				ELSE MIN(spreadsheet.hasOnlinePurchase)
+				END as hasOnlinePurchase,
+			STRUCT(
+				CASE WHEN MIN(spreadsheet.dtcMemberId) <> MAX(spreadsheet.dtcMemberId)
+					THEN ERROR('Non-unique violation: gorillionaire.dtcMemberId; cik:' || cik || ', ticker:' || stockSymbol)
+					ELSE SAFE_CAST(MIN(spreadsheet.dtcMemberId) as INT64) --TODO: TA Outreach to get the right ID for AST
+					END as dtcMemberId,
+				CASE WHEN MIN(spreadsheet.transferAgent) <> MAX(spreadsheet.transferAgent)
+					THEN ERROR('Non-unique violation: gorillionaire.transferAgent; cik:' || cik || ', ticker:' || stockSymbol)
+					ELSE MIN(spreadsheet.transferAgent)
+					END as name,
+				CAST('1900-01-01' AS DATE) as asOfDate,
+				"https://docs.google.com/spreadsheets/d/1MjDad_gZmywTxMVQxsnH2uc9sPIvUa3O51SDyTYm_vs/edit" as docUrl
+				) as transferAgent
+		FROM `apes-on-parade-default.drs_data.gorillionaire_tickers` as spreadsheet
+		WHERE spreadsheet.cik IS NOT NULL
+		GROUP BY 1, 2
+		)
+	, map_nasdaq_dtc_transfer_agents AS (
+		SELECT name as pk1_name,
+			CASE WHEN MIN(selected_id) <> MAX(selected_id) THEN ERROR('Non-unique violation: map_nasdaq_dtc_transfer_agents, name:' || name)
+				ELSE MIN(selected_id) END as dtcMemberId
+		FROM `apes-on-parade-default.src_first_party.map_nasdaq_dtc_transfer_agents`
+		GROUP BY 1
+		)
+	, security_nasdaq as (
+		SELECT
+			security_sec.pk2_cik,
+			security_sec.pk2_ticker,
+		CASE WHEN MIN(nasdaq.CUSIP) <> MAX(nasdaq.CUSIP)
+			THEN ERROR('Non-unique violation: nasdaq.CUSIP; cik:' || pk2_cik || ', ticker:' || pk2_ticker)
+			ELSE MIN(nasdaq.CUSIP)
+			END as cusip,
+		CASE WHEN MIN(nasdaq.Description) <> MAX(nasdaq.Description)
+			THEN ERROR('Non-unique violation: nasdaq.Description; cik:' || pk2_cik || ', ticker:' || pk2_ticker)
+			ELSE MIN(nasdaq.Description)
+			END as description,
+		CASE WHEN MIN(nasdaq.Transfer_Agent) <> MAX(nasdaq.Transfer_Agent)
+			THEN ERROR('Non-unique violation: nasdaq.Transfer_Agent; cik:' || pk2_cik || ', ticker:' || pk2_ticker)
+			ELSE ARRAY_AGG(STRUCT(
+				map.dtcMemberId,
+				ta as name,
+				CAST(Effective_Date AS DATE) as asOfDate,
+				"https://www.nasdaqtrader.com/content/home/help/samplereports/Fundamental_Data_cusip121720.xlsx" as docUrl
+				))
+			END as claimedTransferAgents
+		FROM `apes-on-parade-default.src_unlicensed.nasdaq_fundamentals_sample` as nasdaq
+		LEFT JOIN UNNEST([ SPLIT(LOWER(Issue_Name), SUBSTR(LOWER(Description),1,10))[OFFSET(0)] ]) as impliedIssuerName
+		INNER JOIN security_sec
+			ON  security_sec.exchange = "Nasdaq"
+			AND security_sec.pk2_ticker = nasdaq.Symbol
+			AND stringOverlap(security_sec.issuerName, impliedIssuerName) > 25
+		LEFT JOIN UNNEST(SPLIT(nasdaq.Transfer_Agent, " ; ")) as ta
+		LEFT JOIN map_nasdaq_dtc_transfer_agents as map
+			ON map.pk1_name = ta
+		GROUP BY 1,2
+		)
+	, security_nyse as (
+		SELECT
+			security_sec.pk2_cik,
+			security_sec.pk2_ticker,
+			CASE WHEN MIN(nyse.CUSIP) <> MAX(nyse.CUSIP)
+				THEN ERROR('Non-unique violation: nyse.CUSIP; cik:' || pk2_cik || ', ticker:' || pk2_ticker)
+				ELSE MIN(nyse.CUSIP)
+				END as cusip,
+			CASE WHEN MIN(nyse.Security_Name) <> MAX(nyse.Security_Name)
+				THEN ERROR('Non-unique violation: nyse.Security_Name; cik:' || pk2_cik || ', ticker:' || pk2_ticker)
+				ELSE MIN(nyse.Security_Name)
+				END as description,
+		FROM `apes-on-parade-default.src_unlicensed.nyse_securities` as nyse
+		INNER JOIN security_sec
+			ON  security_sec.exchange = "NYSE"
+			AND security_sec.pk2_ticker = nyse.Stock_Symbol
+			AND stringOverlap(security_sec.issuerName, nyse.Issuer_Name) > 25
+		GROUP BY 1,2
+		)
+	, form14a_transfer_agents as (
+		SELECT
+			sec.pk2_cik,
+			sec.pk2_ticker,
+			ARRAY_AGG(STRUCT(
+				CAST(f14a.transferAgentId as INT64) as dtcMemberId,
+				ta._Participant_Name as name,
+				f14a.filingDate as asOfDate,
+				f14a.htmlUrl as docUrl
+				)) as transferAgents
+		FROM `apes-on-parade-default.src_public.14a_transfer_agents_snapshot` as f14a
+		INNER JOIN security_sec as sec
+			ON sec.pk2_cik = f14a.cik
+		LEFT JOIN `apes-on-parade-default.src_unlicensed.dtc_members_transfer_agents` as ta
+			ON ta.Number = CAST(f14a.transferAgentId AS INT64)
+		GROUP BY 1,2
+		)
 	, claimed_transfer_agents as (
 		SELECT
 			unioned.pk2_cik,
 			unioned.pk2_ticker,
-			ARRAY_AGG(unioned.transferAgent) as claimedTransferAgents,
-			--CASE WHEN MIN=MAX THEN MIN(dtcMemberId)
+			ARRAY_AGG(
+				unioned.transferAgent
+				ORDER BY unioned.transferAgent.asOfDate DESC
+				) as claimedTransferAgents,
+			--CASE WHEN MIN(id)<>MAX(id) THEN warning message
 		FROM (
-			SELECT pk2_cik, pk2_ticker, UNNEST(claimedTransferAgents) as transferAgent FROM security_nasdaq
+			SELECT pk2_cik, pk2_ticker, transferAgent FROM security_gorillionaire WHERE transferAgent.name IS NOT NULL OR transferAgent.dtcMemberId IS NOT NULL
 			UNION ALL
-			SELECT pk2_cik, pk2_ticker, transferAgent FROM security_gorillionaire
-			-- UNION ALL
-			-- FROM issuer_14a...
-		) as unioned
-		ORDER BY unioned.transferAgent.asOfDate DESC
+			SELECT pk2_cik, pk2_ticker, claimedTransferAgents FROM security_nasdaq, security_nasdaq.claimedTransferAgents
+			UNION ALL
+			SELECT pk2_cik, pk2_ticker, transferAgents FROM form14a_transfer_agents, form14a_transfer_agents.transferAgents
+			) as unioned
+		GROUP BY 1,2
 		)
 SELECT
 	security_sec.pk2_cik,
 	security_sec.pk2_ticker,
 	security_sec.exchange,
-	COALESCE(security_nyse.cusip, security_nasdaq.cusip) as cusip,
+	COALESCE(security_nyse.cusip, security_nasdaq.cusip,security_gorillionaire.cusip) as cusip,
 	COALESCE(security_nyse.description, security_nasdaq.description) as description,
-
-
+	ta.claimedTransferAgents[OFFSET(0)] as latestClaimedTransferAgent,
+	ta.claimedTransferAgents as allClaimedTransferAgents
 FROM security_sec
 LEFT JOIN security_nyse
 	ON security_nyse.pk2_cik = security_sec.pk2_cik
@@ -170,3 +191,9 @@ LEFT JOIN security_nyse
 LEFT JOIN security_nasdaq
 	ON security_nasdaq.pk2_cik = security_sec.pk2_cik
 	AND security_nasdaq.pk2_ticker = security_sec.pk2_ticker
+LEFT JOIN security_gorillionaire
+	ON security_gorillionaire.pk2_cik = security_sec.pk2_cik
+	AND security_gorillionaire.pk2_ticker = security_sec.pk2_ticker
+LEFT JOIN claimed_transfer_agents as ta
+	ON ta.pk2_cik = security_sec.pk2_cik
+	AND ta.pk2_ticker = security_sec.pk2_ticker
