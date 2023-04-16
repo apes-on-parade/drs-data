6af7515e0b42458939e7c4051c6e522d20124074
Apes on Parade <88012763+apes-on-parade@users.noreply.github.com>
2023-04-16 06:36:35 +0000

    BQ->React App data build script

--- scripts/transform/react-app-bq/issuers-detail.sql
+++ scripts/transform/react-app-bq/issuers-detail.sql
@@ -1,10 +1,28 @@
--- Use /scripts/common/ndjson-to-files.mjs to reformat BQ output into react-app expected format
-
+WITH transfer_agent as (
+	SELECT
+		pk2_cik as pk1_cik,
+		CASE WHEN MIN(mostSpecificTaIdentifier) = MAX(mostSpecificTaIdentifier)
+			THEN STRUCT(
+				MIN(ta.dtcMemberId) as dtcMemberId,
+				MIN(ta.name) as name
+			)
+			END as transferAgent,
+		ARRAY_AGG(ta) as allClaimedTransferAgents
+	FROM `apes-on-parade-default.dist.securities` as s
+	INNER JOIN UNNEST(s.allClaimedTransferAgents) as ta
+	LEFT JOIN UNNEST([COALESCE(CAST(ta.dtcMemberId AS STRING), ta.name)]) as mostSpecificTaIdentifier
+	WHERE exchange IN ('NYSE','Nasdaq')
+	GROUP BY 1
+	)
 SELECT
-	cik as id,
-	name,
-	transferAgent,
+	i.cik as id,
+	i.name,
+	ta.transferAgent,
+	ta.allClaimedTransferAgents,
 	tickers,
 	holders
-FROM `apes-on-parade-default.dist.issuers`
+FROM `apes-on-parade-default.dist.issuers` as i
+LEFT JOIN transfer_agent as ta
+	ON ta.pk1_cik = i.cik
+WHERE i.is_NN_traded
 ORDER BY holders.totalHolders DESC
--- scripts/transform/react-app-bq/react-app-bq.mjs
+++ scripts/transform/react-app-bq/react-app-bq.mjs
@@ -1,58 +1,104 @@
 import {BigQuery} from '@google-cloud/bigquery'
-import {readOrThrow} from "../../common/index.mjs"
+import {readFile, writeFile, mkdir} from 'node:fs/promises'
+import pathLib from 'path'
 
-const bq = new BigQuery({
-	projectId: 'apes-on-parade-default',
-	keyFilename: './secret/service-account-credentials.json'
-	})
-
-// WIP
-//main()
+main()
 
 async function main() {
 	const location = "US"
+	const bq = new BigQuery({
+		projectId: 'apes-on-parade-default',
+		keyFilename: './secret/service-account-credentials.json'
+		})
+	const cliArgs = process.argv.filter((x,i) => i>=2 || !x.match(/node$|\.mjs$/))
 
 	const queries = [
-		{id:"issuersIndex",		sql:read("issuers-index.sql"),		output: outputIndex("/react-app/dev-data/data/issuers.json")},
-		{id:"issuersDetail",	sql:read("issuers-detail.sql"),		output: outputFiles("/react-app/dev-data/data/issuers")},
-		{id:"brokersIndex",		sql:read("brokers-index.sql"),		output: outputIndex("/react-app/dev-data/data/brokers.json")},
-		{id:"brokersDetail",	sql:read("brokers-detail.sql"),		output: outputFiles("/react-app/dev-data/data/brokers")},
-		{id:"transferAgents",	sql:read("transfer-agents.sql"),	output: outputIndex("/react-app/dev-data/data/transfer-agents.json")},
-		{id:"securitiesIndex",	sql:read("securities-index.sql"),	output: outputIndex("/react-app/dev-data/data/securities.json")},
-		{id:"securitiesDetail",	sql:read("securities-detail.sql"),	output: outputFiles("/react-app/dev-data/data/securities")},
+		//{id:"issuersIndex",		src:"issuers-index.sql",		output: outputIndex("/react-app/dev-data/data/issuers.json")},
+		{id:"issuersDetail",	src:"issuers-detail.sql",		output: outputFiles("/react-app/dev-data/data/issuers/")},
+		//{id:"brokersIndex",		src:"brokers-index.sql",		output: outputIndex("/react-app/dev-data/data/brokers.json")},
+		// {id:"brokersDetail",	src:"brokers-detail.sql",		output: outputFiles("/react-app/dev-data/data/brokers/")},
+		// {id:"transferAgents",	src:"transfer-agents.sql",	output: outputIndex("/react-app/dev-data/data/transfer-agents.json")},
+		// {id:"securitiesIndex",	src:"securities-index.sql",	output: outputIndex("/react-app/dev-data/data/securities.json")},
+		// {id:"securitiesDetail",	src:"securities-detail.sql"),	output: outputFiles("/react-app/dev-data/data/securities/")},
 		]
 
-
-	throw "TODO"
-	//TODO: require cli args to choose which queries to run. If missing, dry-run them and output predicted cost
+	const dryRun = cliArgs.length === 0
+	console.log(`Dry run: ${dryRun}`)
 
 	for(let query of queries){
-		console.log(`Query: ${query.id}`)
+		console.log(`\nQuery: ${query.id}\n####################`)
+		const isSelected = cliArgs.includes(query.id)
+		if(!dryRun && !isSelected){
+			console.log(`	Skipping`)
+			continue
+			}
+		const sql = await read(query.src)
+		console.log(`	SQL query loaded`)
 		const [job] = await bq.createQueryJob({
 			location,
-			query: query.sql
+			query: sql,
+			dryRun
 			})
 		console.log(`	Started as job ${job.id}`)
-		const [rows] = await job.getQueryResults()
-		console.log(`	Received ${rows.length} rows`)
-		await query.output(rows)
+		console.log(`	Status: ${job.metadata.status}`)
+		console.log('	Bytes: ', formatMb(job.metadata.statistics.totalBytesProcessed))
+		console.log('	Cache: ', job.metadata.statistics.query.cacheHit)
+
+		if(!dryRun){
+			const [rows] = await job.getQueryResults()
+			console.log(`	Received: ${rows.length} rows`)
+			await query.output(rows)
+			console.log(`	Output complete`)
+			}
 		}
 	return
 
 }
 
 async function read(file){
-	return await readFile(new URL(file,import.meta.url))
+	return await readFile(new URL(file,import.meta.url), 'utf8')
 	}
 function outputFiles(path){
-	return function(data){
-		throw "TODO"
-		//TODO; copy from /scripts/common/nondjson-to-files.mjs
+	return async function(data){
+		await mkdir(
+			new URL("../../.."+path, import.meta.url),
+			{ recursive: true }
+			)
+		for(let d of data){
+			let id = d.id
+			if(id === undefined){
+				throw "`id` field is required"
+				}
+			await writeFile(
+				new URL(`../../..${path}/${id}.json`, import.meta.url),
+				JSON.stringify(d),
+				{encoding:"utf8"}
+				)
+			}
 		}
 	}
 function outputIndex(path){
-	return function(data){
-		throw "TODO"
-		//TODO; copy from /scripts/common/json-arr-to-json-hash.mjs
+	return async function(data){
+		await mkdir(
+			new URL("../../.."+path.replace(/\/[^\/]+$,""/), import.meta.url),
+			{ recursive: true }
+			)
+		let output = {}
+		for(let d of data){
+			let id = d.id
+			if(id === undefined){
+				throw "`id` field is required"
+				}
+			output[id] = d
+			}
+		await writeFile(
+			new URL(`../../..${path}`, import.meta.url),
+			JSON.stringify(output),
+			{encoding:"utf8"}
+			)
 		}
+	}
+
+function formatMb(bytes) {
+	return Math.ceil(bytes/1e6).toString()+" Mb"
 	}
