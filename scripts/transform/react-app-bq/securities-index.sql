baa1dc3bbe80e4119de7a0e99c9ff1f178c952aa
Apes on Parade <88012763+apes-on-parade@users.noreply.github.com>
2023-05-02 04:57:23 +0000

    DRS request template: Update sort order

--- scripts/transform/react-app-bq/securities-index.sql
+++ scripts/transform/react-app-bq/securities-index.sql
@@ -6,3 +6,6 @@ FROM `apes-on-parade-default.dist.securities` as s
 LEFT JOIN  `apes-on-parade-default.dist.issuers` as i
 	ON i.pk1_cik = s.pk2_cik
 WHERE s.exchange IN ("NYSE","Nasdaq")
+ORDER BY
+	CASE WHEN LENGTH(s.pk2_ticker)<=3 AND s.pk2_ticker NOT LIKE '%-%' THEN 1 ELSE 0 END DESC,
+	i.holders.totalHolders DESC
